826b8ceff91899eaf4e94b5f4ba4c873091655d0
omk-66 <omkalatheeya100@gmail.com>
2025-10-04 15:07:26 +0530

    add some random code (#2)

---
+++ src/components/navber.tsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+    return (
+        <div className="text-2xl font-bold bg-gradient-to-r from-blue-500 to-indigo-500">
+            <div className="container mx-auto px-4">
+                <div className="flex justify-between items-center">
+                    <div className="text-white">Logo</div>
+                    <div className="text-white">Navigation</div>
+                </div>
+            </div>
+        </div>
+    )
+}
